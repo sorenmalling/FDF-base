dab3d77d1d096dcf39ef43abba00bee77015ed07
Soren Malling <soren.malling@gmail.com>
2013-01-05 12:52:51 +0100

    [TASK] Only read backend_layout from rootpage of a branch

--- ext_tables.php
+++ ext_tables.php
@@ -8,5 +8,8 @@ t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript/Base/', 'FDF: De
 TYPO3\CMS\Core\Utility\GeneralUtility::loadTCA('sys_template');
 $GLOBALS['TCA']['sys_template']['columns']['sitetitle']['config']['eval'] = 'required';
 
+TYPO3\CMS\Core\Utility\GeneralUtility::loadTCA('pages');
+$GLOBALS['TCA']['pages']['columns']['backend_layout']['config']['foreign_table_where'] = 'AND backend_layout.pid = ###SITEROOT###';
+
 
 ?>
