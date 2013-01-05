cd2e9e184fb24e60fca3b8d020078e53eb4fe622
Soren Malling <soren.malling@gmail.com>
2013-01-05 12:31:59 +0100

    [TASK] Set sitetitle as required in TCA

--- ext_tables.php
+++ ext_tables.php
@@ -4,5 +4,9 @@ if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 	# Add typoscript templates
 t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript/Base/', 'FDF: Default site');
 
+	# Modify sys_template settings
+TYPO3\CMS\Core\Utility\GeneralUtility::loadTCA('sys_template');
+$GLOBALS['TCA']['sys_template']['columns']['sitetitle']['config']['eval'] = 'required';
+
 
 ?>
