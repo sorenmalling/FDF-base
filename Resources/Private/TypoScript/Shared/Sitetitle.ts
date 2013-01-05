c73dc7145258a6d4993c4eeadda03dd6382f8c1d
Soren Malling <soren.malling@gmail.com>
2013-01-05 12:28:54 +0100

    [FEATURE] Use sitetile from sys_template as sitename
    As we are going to create a root template, we will use it's
    sitetitle field for setting it in the frontend.
    
    At the same time, the field is set as required in TCA

---
+++ Resources/Private/TypoScript/Shared/Sitetitle.ts
@@ -0,0 +1,5 @@
+lib.shared.sitetitle = TEXT
+lib.shared.sitetitle {
+	data = DB:sys_template:1:sitetitle
+	wrap =  |
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -3,66 +3,13 @@
 
 		// unserializing the configuration so we can use it here:
 	$_EXTCONF = unserialize($_EXTCONF);
-/*
-	t3lib_extMgm::addPageTSConfig('
-		mod.web_txtemplavoilaM1.enableDeleteIconForLocalElements = 1
-		TCEFORM.pages {
-			layout.disabled = 1
-			newUntil.disabled = 1
-			content_from_pid.disabled = 1
-			storage_pid.disabled = 1
-			TSconfig.disabled = 1
-			author.disabled = 1
-			author_email.disabled = 1
-			lastUpdated.disabled = 1
-			media.disabled = 1
-			url_scheme.disabled = 1
-			target.disabled = 1
-			alias.disabled = 1
-			subtitle.disabled = 1
-			fe_login_mode.disabled = 1
-			tx_realurl_pathoverride.disabled = 1
-			tx_realurl_exclude.disabled = 1
-			cache_timeout.disabled = 1
-			cache_tags.disabled = 1
-			l18n_cfg.disabled = 1
-			php_tree_stop.disabled = 1
-		}
-		TCEFORM.tt_content {
-			colPos.disabled = 1
-			sys_language_uid.disabled = 1
-			header_position.disabled = 1
-			header_layout.disabled = 1
-			date.disabled = 1
-			rte_enabled.disabled = 1
-			image_zoom.disabled = 1
-			spaceBefore.disabled = 1
-			spaceAfter.disabled = 1
-			imageborder.disabled = 1
-			image_compression.disabled = 1
-			image_effects.disabled = 1
-			imagecaption_position.disabled = 1
-			image_noRows.disabled = 1
-			sectionIndex.disabled = 1
-			linkToTop.disabled = 1
-			layout.disabled = 1
-			section_frame.disabled = 1
 
-			imageorient {
-				removeItems = 17,18
-			}
-			CType {
-				removeItems = header,table,bullets,div,menu,uploads,search,multimedia
-			}
-		}
-		mod {
-			web_list {
-				hideTables = static_territories,static_countries,static_country_zones,static_languages,static_currencies,sys_action,tt_content,pages
-			}
-		}
+	t3lib_extMgm::addPageTSConfig('
+		<INCLUDE_TYPOSCRIPT: source="FILE:EXT:fdf_base/Resources/Private/TSConfig/Setup.ts">
+		mod.web_txtemplavoilaM1.enableDeleteIconForLocalElements = 1
 		templavoila.wizards.newContentElement.wizardItems.common.show := removeFromList(bullets,table)
 	');
-*/
+
 	t3lib_extMgm::addUserTSConfig('
 		mod.web_txtemplavoilaM1.sideBarEnable = 0
 	');
